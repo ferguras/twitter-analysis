b176b073c7215760e51d52e4fa6db647b35a225d
ferguras <github@solarwar.org>
2018-03-20 19:51:15 +0000

    New line adjustment

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 session = HTMLSession()
 
-
 def get_tweets(user, tweets=100, retweets=True, maxpages=25):
     """Gets tweets for a given user, via the Twitter frontend API."""
 
